04dce40e0c243443bdc723afb0b5a7c40e908044
Francisco Pereira <francisco.pereira16@inacapmail.cl>
2024-10-17 21:09:00 -0300

    fix temperature conversion to integer

--- main.js
+++ main.js
@@ -61,7 +61,7 @@ document.querySelector('form').addEventListener('submit', async (event) => {
 
     console.log(data)
     // convertir la temperatura de kelvin a celsius los grados kelvin son 273.15 
-    const temp = (data.main.temp - 273.15).toFixed(2)
+    const temp = (data.main.temp - 273.15).toFixed(0) // convertir a entero
     document.getElementById('temp').innerHTML = `Temperatura: ${temp}°C`
     document.getElementById('humidity').innerHTML = `Humedad: ${data.main.humidity}%`
 
